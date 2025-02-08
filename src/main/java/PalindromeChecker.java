f33a837a0848e67d5e142348dc3dbd6799cac67b
Ny09-sand <nicholas.yangwt@gmail.com>
2025-02-08 14:32:27 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -1,26 +1,42 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+public class PalindromeChecker {
+public void tester()
 public void setup()
 {
+  //String lines[] = loadStrings("palindromes.txt");
+  String[] lines = new String[6]; 
+    try{
+        File myFile = new File("palindromes.txt");
+        Scanner myReader = new Scanner(myFile);
+        int counter = 0;
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            lines[counter] = data;
+            counter++;
+        }
+        myReader.close();
+    }
+    catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
   String lines[] = loadStrings("palindromes.txt");
   System.out.println("there are " + lines.length + " lines");
   for (int i=0; i < lines.length; i++) 
   {
-    if(palindrome(lines[i])==true)
-    {
-      System.out.println(lines[i] + " IS a palindrome.");
-    }
-    else
-    {
-      System.out.println(lines[i] + " is NOT a palindrome.");
-    }
-  }
+@@ -37,16 +16,18 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
+  //your code here
+   return (word.equals(reverse(word))); 
   return (word.equals(reverse(word))); 
-
 }
 public String reverse(String str)
 {
+     String s = new String("");
     String s = new String("");
     if (str.length()==0) {return "";} 
     for ( int i = str.length()-1; i >= 0 ; i --){
@@ -30,4 +46,5 @@ public String reverse(String str)
       } 
     }
     return s;
+}
 }
